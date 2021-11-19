520a0d8ac7cb1b798477995054b7086fa4dcfb8d
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-19 23:11:26 +0800

    feat(app): init MikroORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Module, NotFoundException } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { DB_PATH } from './env.constants';
 
 @Module({
   imports: [
+    MikroOrmModule.forRoot({
+      type: 'sqlite',
+      dbName: DB_PATH,
+      autoLoadEntities: true,
+      forceUndefined: true,
+      findOneOrFailHandler: () => new NotFoundException(),
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
